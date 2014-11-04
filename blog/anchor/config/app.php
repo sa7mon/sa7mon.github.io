9f77f7a4aabc97c9e1f41d48a89dda378650682c
Dan Salmon <dan.sa7mon@gmail.com>
2014-11-03 20:36:19 -0600

    Install Anchor CMS

---
+++ blog/anchor/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'url' => '/personal-site/blog',
+	'index' => '',
+	'timezone' => 'America/Chicago',
+	'key' => 'OaFa4o3bQcj5sDtCun8HxVzN4ngbRjCj',
+	'language' => 'en_GB',
+	'encoding' => 'UTF-8'
+);
