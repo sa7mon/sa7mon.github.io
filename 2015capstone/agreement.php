5673bb282da876bf8a6672ac42f4a47388d34a49
Dan Salmon <dan.sa7mon@gmail.com>
2015-12-07 18:02:17 -0600

    Add SCC capstone project.

---
+++ 2015capstone/agreement.php
@@ -0,0 +1,62 @@
+<html>
+	<head>
+		<title>Capstone - Agreement</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="page-wrapper">
+			<div id="header">
+				<h1>Capstone Project <small>An Inventory Tracking System</small></h1>
+			</div>
+			<div id="sidebar">
+				<h2 class="title">Pages</h2>
+				<ul>
+					<li><a href="index.php">Executive Summary</a></li>
+					<li><a href="#">Internship Agreement</a></li>
+					<li><a href="time.php">Time Sheets</a></li>
+					<li><a href="evaluation.php">Evaluation</a></li>
+					<li><a href="reflection.php">Reflection</a></li>
+				</ul>
+			</div>
+			<div id="page-content">
+				<h1 class="page-title">Internship Agreement</h1>
+				<h2 class="title">Responsibilities of the Intern</h2>
+				<ol id="responsibilities">
+					<li>The intern will keep track of hours worked using an Excel spreadsheet.</li>
+					<li>The intern will show honesty, punctuality, courtesy, respect for life and property, a cooperative attitude, proper health and grooming habits, good dress, and a willingness to learn.</li>
+					<li>
+						The intern will furnish the coordinating instructor with all necessary information and complete all necessary reports.
+					</li>
+					<li>
+						The intern will conform to the rules and regulations of the internship site and will consult with the coordinating instructor about any difficulties arising at the internship site.
+					</li>
+					<li>
+						The intern shall not terminate his/her employment without proper notification to his/her internship coordinator and employer.
+					</li>
+					<li>
+						For health programs, Minnesota law requires that any person who provides services that involve direct contact at a facility licensed by the Minnesota Department of Health have a background study conducted by the state. An individual who is disqualified from having direct contact as a result of the background study, and whose disqualification is not set aside by the Commissioner of Health, will not be permitted to participate in internship placement.
+					</li>
+				</ol>
+
+				<h2 class="title">Responsibilities of the Company</h2>
+				<ol>
+					<li>
+						The Internship Supervisor will employ the student for at least the minimum number of hours for the entire agreed upon training period. This period can be extended depending on the company's needs and requirements.
+					</li>
+					<li>The Internship Supervisor will not terminate the employment of the intern without consulting with the coordinating instructor.</li>
+					<li>The Internship Supervisor will consult with the coordinating instructor about any difficulties arising at the internship site.</li>
+					<li>The Internship Supervisor will assist in the evaluation of the intern and consult with the coordinating instructor concerning the intern.</li>
+					<li>The internship site will not discrimination against students in recruitment, hiring, placement, assignments, level of responsibility, and pay, on the basis of race, sex, color, creed, religion, age, national origin, disability, marital status, status with regard to public assistance, sexual orientation, or membership or activity in a local commission.</li>
+				</ol>
+				<h2 class="title">Responsibilities of the Instructor</h2>
+				<ol>
+					<li>The coordinating instructor will be in contact with the Internship Supervisor and assist in the evaluation of the intern.</li>
+					<li>The coordinating instructor will assist and consult with the intern concerning his/her training.</li>
+				</ol>
+			</div>
+		</div>
+		<div id="footer">
+				<p>&copy; 2015 Dan Salmon - <a href="https://danthesalmon.com/">danthesalmon.com</a></p>
+			</div>
+	</body>
+</html>
---
+++ 2015capstone/reflection.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Capstone Project</title>
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="page-wrapper">
+			<div id="header">
+				<h1>Capstone Project <small>An Inventory Tracking System</small></h1>
+			</div>
+			<div id="sidebar">
+				<h2 class="title">Pages</h2>
+				<ul>
+					<li><a href="index.php">Executive Summary</a></li>
+					<li><a href="agreement.php">Internship Agreement</a></li>
+					<li><a href="time.php">Time Sheets</a></li>
+					<li><a href="evaluation.php">Evaluation</a></li>
+					<li><a href="#">Reflection</a></li>
+				</ul>
+			</div>
+			<div id="page-content">
+			</div>
+		</div>
+		<div id="footer">
+				<p>&copy; 2015 Dan Salmon - <a href="https://danthesalmon.com/">danthesalmon.com</a></p>
+			</div>
+	</body>
+</html>
