bd7bcdf8c1edaff25441c1f36284db96a6cdb74e
Dan Salmon <sa7mon@users.noreply.github.com>
2026-02-11 22:13:58 -0600

    Improve Slug Generation (#97)
    * move slug generation to shell script
    
    * strip multiple hyphens
    
    * add make-slug unit tests

---
+++ .github/scripts/make-slug.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# $# - number of arguments = flags * 2
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 -t <title> -d <date>"
+    echo "       date format: YYYY-MM-DD"
+    exit 1
+fi
+
+while getopts t:d: flag
+do
+    case "${flag}" in
+        t) title=${OPTARG};;
+        d) date=${OPTARG};;
+    esac
+done
+
+# lowercase all characters, replace spaces with hyphens, remove non-alphanumeric characters except hyphens, then replace 
+# multiple hyphens with a single hyphen
+export slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g' | sed 's/[^a-z0-9-]//g' | sed -E 's/\-+/\-/g')
+export FILE_SLUG="$(date +'%Y-%m-%d')_${slug}_$date"
+echo "$FILE_SLUG"
