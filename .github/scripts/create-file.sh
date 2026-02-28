2c1a4de41f13cc4efec96d22df282ded747fb757
Dan Salmon <dan@salmon.cat>
2026-02-27 23:43:38 -0600

    add create-file.sh

---
+++ .github/scripts/create-file.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# $# - number of arguments = flags * 2
+if [ "$#" -ne 12 ]; then
+    echo "Usage: $0 -i <title> -s <slug> -u <url> -t <tags> -d <description> -f <format>"
+    exit 1
+fi
+
+while getopts i:s:u:t:d:f: flag
+do
+    case "${flag}" in
+        i) title=${OPTARG};;
+        s) slug=${OPTARG};;
+        u) url=${OPTARG};;
+        t) tags=${OPTARG};;
+        d) desc=${OPTARG};;
+        f) format=${OPTARG};;
+    esac
+done
+
+echo "args:"
+for i; do 
+   echo $i 
+done
+
+hugo new links/$slug.md
+export FILE="content/links/$slug.md"
+export SITE=$(echo $url | awk -F/ '{print $3}')
+gsed -i -E "s/title: .+$/title: \"$title\"/" $FILE
+gsed -i -E "s/site: $/site: $SITE/" $FILE
+gsed -i -E "s/link_tags: .+$/link_tags: $tags/" $FILE
+gsed -i -E "s/formats: .+$/formats: $format/" $FILE
+printf "\n$url\n$desc" >> $FILE
+cat $FILE
